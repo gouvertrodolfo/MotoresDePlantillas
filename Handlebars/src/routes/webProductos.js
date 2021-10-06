33b6427940e8f5d248207a7b54a8d9f8484aea89
Rodolfo <rgouvert@gmail.com>
2021-10-06 10:35:20 -0300

    tochi

--- Handlebars/src/routes/webProductos.js
+++ Handlebars/src/routes/webProductos.js
@@ -7,15 +7,15 @@ const Contenedor = require("../Contenedor")
 const inventario =new Contenedor('productos.txt')
 /* -------------------------------------- */
 
-// webProductos.get("/",(req,res)=>{
-//     res.render('formulario', { titulo:"Agregar Producto"})
-// })
+webProductos.get("/",(req,res)=>{
+     res.render('formulario.hbs', { titulo:"Agregar Producto"})
+})
 
-// webProductos.post("/", async (req, res) => {
-//         let producto = req.body
-//         const id = await inventario.save(producto)
-//         res.redirect('/productos/Listado')
-// })
+webProductos.post("/", async (req, res) => {
+         let producto = req.body
+         const id = await inventario.save(producto)
+         res.redirect('/productos/Listado')
+})
 
 webProductos.get("/Listado", async (req,res)=>{
     let items = await inventario.getAll()
